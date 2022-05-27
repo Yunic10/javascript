218ed4ca265495af3582bce2ae55fda0484b07dc
Yunic10 <100017240+Yunic10@users.noreply.github.com>
2022-05-27 23:00:54 +0700

    Create jumlah.js

---
+++ jumlah.js
@@ -0,0 +1,18 @@
+/**
+ * jumlahkan semua angka dan string yang menjadi parameter
+ */
+
+function sum(param) {
+  param = parseInt(param);
+  ;
+  return param;
+  };
+/**
+ * contoh
+ * sum(1,2,'3','4')
+ * 10
+ */
+
+console.log(sum(3, 6, "3", "2", 5, 7));
+console.log(sum("5", 5, "2", 2));
+console.log(sum("123123123"));
